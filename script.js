af911528bb707cb21acced5087c3aeaa604209b4
Siham <sihambordjah1@gmail.com>
2025-11-10 18:06:00 +0100

    Mise à jour du site

---
+++ script.js
@@ -0,0 +1,50 @@
+const slides = document.querySelectorAll(".slide");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+}
+
+next.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+prev.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
+
+// Changement automatique toutes les 5 secondes
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 5000);
+// Changement automatique toutes les 5 secondes
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 5000);
+
+//Formulaire de contact
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
