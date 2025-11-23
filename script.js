8b7d38d01d837d66f2879337653614bd0dffd3e4
Siham <sihambordjah1@gmail.com>
2025-11-23 08:46:33 +0100

    ajout menu humburger

--- script.js
+++ script.js
@@ -72,3 +72,16 @@ form.addEventListener("submit", async (e) => {
     status.className = "error";
   }
 });
+
+//menu hamburger responsive mobile
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  const menutoggle = document.querySelector(".menutoggle");
+  const navbar = document.querySelector(".navbar");
+  menutoggle.classList.toggle("active");
+  navbar.classList.toggle("active");
+}
